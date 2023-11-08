bee0d95bec7f5c84176b8f10d74b7a94028f48d3
Andrew <andrewozo123@gmail.com>
2023-11-08 11:36:29 -0500

    finished movie class

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,24 @@
 // import the Media class:
+const Media = require("./Media");
 
 // create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating) {
+    super(title, year, genre);
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static longestMovie(movies) {
+    movies.sort((a, b) => b.duration - a.duration);
+    return movies[0];
+  }
+}
 
 // don't change below
 module.exports = Movie;
